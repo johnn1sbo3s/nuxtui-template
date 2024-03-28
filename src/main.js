5e2115c1b624ea51b1522f4c4635f08543011735
johnn1sbo3s <joaopaulosncastro@gmail.com>
2024-03-28 13:43:14 -0300

    -- RESETANDO O REPOSITÓRIO

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import PrimeVue from 'primevue/config';
-
-import 'primevue/resources/themes/lara-light-purple/theme.css'
-
-createApp(App).use(PrimeVue).use(router).mount('#app')
