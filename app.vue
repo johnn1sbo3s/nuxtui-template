157ab23946d153dc4c45e296c1bf28ea0450dff6
john1sbo3s <joaopaulosncastro@gmail.com>
2024-03-28 21:37:41 -0300

    sei lá

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <h1 class="text-3xl bg-red-500 text-white p-4">XAMA NA CARANGA</h1>
+  <div class="bg-violet-700 p-2 w-full h-12">
+    <!-- <div v-for="link in links" :key="index" class="bg-white">
+      <NuxtLink :to="link.path">{{ link.name }}</NuxtLink>
+    </div> -->
   </div>
+  <NuxtPage />
 </template>
+
+<script setup>
+
+const links = ref([
+  {
+    name: 'Home',
+    path: '/'
+  },
+  {
+    name: 'About',
+    path: '/about'
+  }
+])
+</script>
+
+<style>
+</style>
