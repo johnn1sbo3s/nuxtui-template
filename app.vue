97fc15a00abb17008f9344db519fea711ad22168
john1sbo3s <joaopaulosncastro@gmail.com>
2024-03-29 12:49:47 -0300

    finaliza configuração do NuxtUI

--- app.vue
+++ app.vue
@@ -3,23 +3,14 @@
     <h1 class="font-bold px-4 py-5">This is the project start</h1>
   </div>
   <div class="m-4 flex justify-center">
-    <UButton label="Button" size="md"/>
+    <NuxtPage />
   </div>
 </template>
 
 <script setup>
 
-const links = ref([
-  {
-    name: 'Home',
-    path: '/'
-  },
-  {
-    name: 'About',
-    path: '/about'
-  }
-])
 </script>
 
 <style>
+
 </style>
